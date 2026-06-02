6fb857d429d5c9c6e6dd17d12abd1f366ebb7af8
Renan Marozzi <remarozzi25@gmail.com>
2026-06-02 16:20:47 -0300

    fix: remove rewrite que bloqueava API routes

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Proxy to backend (used in development)
-  async rewrites() {
-    const backendUrl = process.env.BACKEND_URL || "http://localhost:3001";
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${backendUrl}/:path*`,
-        // This is overridden by the API Route files in production
-      },
-    ];
-  },
-};
-
+const nextConfig = {};
 module.exports = nextConfig;
